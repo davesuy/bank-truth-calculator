8c44786adde00369fbe605f7865a0544ed5afcb9
Dave <dave.r@arizto.co.nz>
2025-11-11 18:47:34 +0800

    Seeder fixed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // Only seed banks data, no test users needed
+        $this->call([
+            BankSeeder::class,
         ]);
     }
 }
